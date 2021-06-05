002538b61ffdd07c56a9efbe7d5d160e0e8cf50a
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-05 19:50:48 +0530

    Implementation of queue using array

--- src/queue/SinglyQueueArray.java
+++ src/queue/SinglyQueueArray.java
@@ -1,15 +1,21 @@
 package queue;
 
+import java.util.Scanner;
+
 public class SinglyQueueArray {
     int[] arr;
     int rear, front;
+    static Scanner scanner = new Scanner(System.in);
     public SinglyQueueArray(){
         arr = new int[10];
         rear = -1;
         front = -1;
     }
     public void enqueue(){
-
+        int data = scanner.nextInt();
+        if(rear == arr.length-1){
+            System.out.println("Queue is Full!!");
+        }
     }
     public  void dequeue(){
 
