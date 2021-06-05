d40a8b831b5388282c2d773093f41b4379793cc7
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-05 19:46:40 +0530

    Implementation of queue using array

---
+++ src/queue/SinglyQueueArray.java
@@ -0,0 +1,11 @@
+package queue;
+
+public class SinglyQueueArray {
+    int[] arr;
+    int rear, front;
+    public SinglyQueueArray(){
+        arr = new int[10];
+        rear = -1;
+        front = -1;
+    }
+}
