8e52f4e59ff710ed949572cadb4dadc7550ac2df
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-06 21:41:44 +0530

    Implementation of circular queue using array

--- src/queue/CircularQueueArray.java
+++ src/queue/CircularQueueArray.java
@@ -1,5 +1,12 @@
 package queue;
 
 public class CircularQueueArray {
+    int [] arr;
+    int front, rear;
+    public CircularQueueArray(){
+        arr = new int[10];
+        front = -1;
+        rear = -1;
+    }
 
 }
