fae5b72919b9e50e7b25cc4552cd5e55c4e86a3e
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-06 21:44:57 +0530

    Implementation of circular queue using array

--- src/queue/CircularQueueArray.java
+++ src/queue/CircularQueueArray.java
@@ -1,12 +1,33 @@
 package queue;
 
+import java.util.Scanner;
+
 public class CircularQueueArray {
     int [] arr;
     int front, rear;
+    static Scanner scanner = new Scanner(System.in);
     public CircularQueueArray(){
         arr = new int[10];
         front = -1;
         rear = -1;
     }
+    public void enqueue(){
+        if((rear == arr.length-1 && front == 0) || (rear == front-1)){
+            System.out.println("Circular Queue is full!!!");
+        } else {
+            System.out.println("Enter data:");
+            int data = scanner.nextInt();
 
+            if(front == -1){
+                front=0;
+            }
+            if (rear < arr.length-1){
+                rear++;
+            } else if(rear == arr.length-1 && front != 0){
+                rear=0;
+            }
+            arr[rear] = data;
+            System.out.println("Data inserted successfully!!!");
+        }
+    }
 }
