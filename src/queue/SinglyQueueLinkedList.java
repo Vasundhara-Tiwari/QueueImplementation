9c54e453b84da33c228ff9946d861c55def99352
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-08 13:38:09 +0530

    Implementation of singly queue using linked list

--- src/queue/SinglyQueueLinkedList.java
+++ src/queue/SinglyQueueLinkedList.java
@@ -3,7 +3,61 @@ package queue;
 import java.util.Scanner;
 
 public class SinglyQueueLinkedList {
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
     Node front;
     Node rear;
+    public  SinglyQueueLinkedList(){
+        front = null;
+        rear = null;
+    }
+    public void enqueue(){
+        Node newNode = new Node();
+        newNode.data = scanner.nextInt();
+        newNode.next = null;
+
+        if (front == null || rear == null){
+            front = newNode;
+            rear = newNode;
+        } else {
+            Node current = front;
+
+            while (current.next !=null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        System.out.println("Data inserted successfully!!!");
+    }
+    public void dequeue(){
+        if(front == null || rear == null){
+            System.out.println("Queue empty!!!");
+        } else {
+            System.out.println("Element deleted : "+front.data);
+            front = front.next;
+        }
+    }
+    public void traverse(){
+        Node current;
+        if(front == null || rear == null){
+            System.out.println("Singly Queue is empty!!!");
+        } else {
+            for(current = front; current != null; current = current.next){
+                System.out.println(" "+current.data);
+            }
+        }
+    }
+    public void peek(){
+        if( front == null || rear == null){
+            System.out.println("Queue is empty!!!");
+        } else {
+            System.out.println("Last element "+front.data);
+        }
+    }
+    public void poll(){
+        if( front == null || rear == null){
+            System.out.println("Queue is empty!!!");
+        } else {
+            System.out.println("Last element "+rear.data);
+        }
+    }
 }
