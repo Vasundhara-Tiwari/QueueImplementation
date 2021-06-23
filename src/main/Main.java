58be80b964c06f06ce30f19c0248199db91c3380
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-23 23:06:22 +0530

    Creating object for the required classes

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,14 @@ package main;
 
 import queue.CircularQueueArray;
 import queue.SinglyQueueArray;
+import queue.SinglyQueueLinkedList;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        SinglyQueueArray obj1 = new SinglyQueueArray();
+        SinglyQueueLinkedList obj2 = new SinglyQueueLinkedList();
+        CircularQueueArray obj3 = new CircularQueueArray();
     }
 }
