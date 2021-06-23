3c2de3c1db26c4a69d2ab48c5f13b5d5c8d66298
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-23 23:06:59 +0530

    Creating working main method, for performing the required operations

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,5 +11,9 @@ public class Main {
         SinglyQueueArray obj1 = new SinglyQueueArray();
         SinglyQueueLinkedList obj2 = new SinglyQueueLinkedList();
         CircularQueueArray obj3 = new CircularQueueArray();
+        System.out.println("Enter you choice:");
+        System.out.println("Press 1 for Singly Queue Array");
+        System.out.println("Press 2 for Singly Queue Linked List");
+        System.out.println("Press 3 for Circular Queue");
     }
 }
