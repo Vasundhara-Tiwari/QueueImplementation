e28fc593ba6f27b301eb3f98eacefcc3d36c2cf5
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-06-23 23:07:08 +0530

    Creating working main method, for performing the required operations

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,5 +15,7 @@ public class Main {
         System.out.println("Press 1 for Singly Queue Array");
         System.out.println("Press 2 for Singly Queue Linked List");
         System.out.println("Press 3 for Circular Queue");
+        Scanner scanner = new Scanner(System.in);
+        int c = scanner.nextInt();
     }
 }
